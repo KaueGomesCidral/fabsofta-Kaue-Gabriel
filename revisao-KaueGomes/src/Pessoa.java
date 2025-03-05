1e13189697cd5e763b914a4af74b05a5eb12e344
KaueGomesCidral <kauegomescidral@gmail.com>
2025-03-05 20:27:41 -0300

    feat: revisao

---
+++ revisao-KaueGomes/src/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa { // Formas
+    private String nome;  //Variavel - Atributo - Propriedade  // <- Informação
+
+
+    //Método
+    public String getNome() {
+        return nome;  // <- Ação - Comportamento
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
