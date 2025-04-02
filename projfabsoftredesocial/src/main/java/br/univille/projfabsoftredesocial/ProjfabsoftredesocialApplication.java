cb78f940280e0dbf603134a42c53c4ebce4833eb
KAUE GOMES CIDRAL <kaue.cidral@univille.br>
2025-04-01 22:03:34 -0300

    começo do trabalho

---
+++ projfabsoftredesocial/src/main/java/br/univille/projfabsoftredesocial/ProjfabsoftredesocialApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftredesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftredesocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftredesocialApplication.class, args);
+	}
+
+}
---
+++ projfabsoftredesocial/src/test/java/br/univille/projfabsoftredesocial/ProjfabsoftredesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftredesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftredesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
