c97cb0c4f4f5b0cd176a89c8ef09f5adfac7ab27
GABRIEL FERNANDES DA COSTA <gabriel.fernandes.da.costa@univille.br>
2025-04-22 22:05:31 -0300

    finalizacao

--- projfabsoftredesocial/src/main/java/br/univille/projfabsoftredesocial/controller/UsuarioController.java
+++ projfabsoftredesocial/src/main/java/br/univille/projfabsoftredesocial/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.univille.projfabsoftredesocial.entity.Usuario;
 import br.univille.projfabsoftredesocial.service.UsuarioService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -27,5 +30,40 @@ public class UsuarioController {
         
         return new ResponseEntity<>(listaUsuarios, HttpStatus.OK);
     }
+    @PostMapping
+    public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario){
+        if(usuario == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if(usuario.getId() == 0){
+            service.save(usuario);
+            return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> putUsuario(@PathVariable long id, @RequestBody Usuario usuario){
+        if(id <= 0 || usuario == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var usuarioAntigo = service.getById(id);
+        if (usuarioAntigo == null){
+            return ResponseEntity.notFound().build();
+        }
 
+        usuarioAntigo.setNome(usuario.getNome());
+        usuarioAntigo.setCurso(usuario.getCurso());
+        usuarioAntigo.setSemestreTurma(usuario.getSemestreTurma());
+        usuarioAntigo.setTelefone(usuario.getTelefone());
+        usuarioAntigo.setEmail(usuario.getEmail());
+        usuarioAntigo.setSenha(usuario.getSenha());
+        usuarioAntigo.setBio(usuario.getBio());
+        usuarioAntigo.setAtletica(usuario.getAtletica());
+        usuarioAntigo.setDataNascimento(usuario.getDataNascimento());
+        usuarioAntigo.setPostagens(usuario.getPostagens());
+        usuarioAntigo.setAmigos(usuario.getAmigos());
+
+        service.save(usuarioAntigo);
+        return new ResponseEntity<Usuario>(usuarioAntigo, HttpStatus.OK);
+    }
 }
