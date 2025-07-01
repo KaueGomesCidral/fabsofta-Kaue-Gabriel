e1201180aa0d27e2913d44872bf7b0176e8f90e3
Gabriel Fernandes da Costa <142421247+Gabrielfercosta@users.noreply.github.com>
2025-07-01 21:46:15 +0000

    att

--- projfabsoft_frontend/src/app/postagem/postagem.component.ts
+++ projfabsoft_frontend/src/app/postagem/postagem.component.ts
@@ -1,20 +1,43 @@
 import { Component } from '@angular/core';
 import { Postagem } from '../model/postagem';
 import { PostagemService } from '../service/postagem.service';
+import { FormsModule } from '@angular/forms'; // Importação do FormsModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-postagem',
+  standalone: true, // Indica que o componente é standalone
+  imports: [FormsModule, CommonModule], // Adiciona o FormsModule aqui
   templateUrl: './postagem.component.html',
   styleUrls: ['./postagem.component.css'],
 })
 export class PostagemComponent {
   listaPostagens: Postagem[] = [];
+  textoPostagem: string = '';
 
   constructor(private postagemService: PostagemService) {}
 
   ngOnInit(): void {
     this.postagemService.getPostagens().subscribe((resposta) => {
       this.listaPostagens = resposta;
+    });
+  }
+
+  publicarPost(): void {
+    if (!this.textoPostagem.trim()) {
+      return;
+    }
+
+    const novaPostagem: Postagem = {
+      id: 0, 
+      texto: this.textoPostagem,
+      autorId: 2,
+      curtidas: [],
+    };
+
+    this.postagemService.savePostagem(novaPostagem).subscribe((resposta) => {
+      this.listaPostagens.unshift(resposta); // Adiciona a nova postagem no topo da lista
+      this.textoPostagem = ''; // Limpa o campo de texto
     });
   }
 }
--- projfabsoft_frontend/src/app/service/postagem.service.ts
+++ projfabsoft_frontend/src/app/service/postagem.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Postagem } from '../model/postagem';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,22 +11,22 @@ export class PostagemService {
 
   constructor(private http: HttpClient) {}
 
-  getPostagens() {
+  getPostagens(): Observable<Postagem[]> {
     return this.http.get<Postagem[]>(this.apiURL);
   }
 
-  savePostagem(postagem: Postagem) {
+  savePostagem(postagem: Postagem): Observable<Postagem> {
     if (postagem.id) {
-      return this.http.put(`${this.apiURL}/${postagem.id}`, postagem);
+      return this.http.put<Postagem>(`${this.apiURL}/${postagem.id}`, postagem);
     }
-    return this.http.post(this.apiURL, postagem);
+    return this.http.post<Postagem>(this.apiURL, postagem);
   }
 
-  getPostagemById(id: number) {
+  getPostagemById(id: number): Observable<Postagem> {
     return this.http.get<Postagem>(`${this.apiURL}/${id}`);
   }
 
-  excluirPostagem(id: number) {
-    return this.http.delete(`${this.apiURL}/${id}`);
+  excluirPostagem(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiURL}/${id}`);
   }
 }
